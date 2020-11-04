3c18a30e6707b3d0843b16da1a7013ac44623586
LeoSanGo <souoleo@gmail.com>
2020-11-03 23:32:21 -0300

    Commit classes indexController, adicionado dados do banco via terminal

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -1,16 +1,19 @@
 package com.carros.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
 	
-	@GetMapping
-	public String hello() {
-		return "Hello Spring Boot";
+	@GetMapping()
+	public String get() {
+		return "API dos carros";
 	}
+	
 
 }

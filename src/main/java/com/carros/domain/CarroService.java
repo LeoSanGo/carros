b5f7d7e9736353eb63d833d38cdfd8d1fbf833af
LeoSanGo <souoleo@gmail.com>
2020-11-07 14:15:26 -0300

    Adicionado o DTO

--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -2,10 +2,13 @@ package com.carros.domain;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+
+import com.carros.domain.dto.CarroDTO;
 
 @Service
 public class CarroService {
@@ -13,8 +16,8 @@ public class CarroService {
 	@Autowired
 	private CarroRepository rep;
 
-	public Iterable<Carro> getCarros() {
-		return rep.findAll();
+	public List<CarroDTO> getCarros() {
+		return rep.findAll().stream().map(CarroDTO::new).collect(Collectors.toList());
 	}
 
 	public Optional<Carro> getCarroById(Long id) {
@@ -22,9 +25,9 @@ public class CarroService {
 		return rep.findById(id);
 	}
 	
-	public List<Carro> getCarrosByTipo(String tipo) {
+	public List<CarroDTO> getCarrosByTipo(String tipo) {
 		
-		return rep.findByTipo(tipo);
+		return rep.findByTipo(tipo).stream().map(CarroDTO::new).collect(Collectors.toList());
 	}
 	
 	public Carro insert(Carro carro) {

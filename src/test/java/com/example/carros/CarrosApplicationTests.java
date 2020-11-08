853995d9a80f1f2a091c3cb04b4346242f0e50e6
LeoSanGo <souoleo@gmail.com>
2020-11-08 19:25:42 -0300

    Inserido testes e resolução erro (@SpringBootTest(classes = CarrosApplication.class))

--- src/test/java/com/example/carros/CarrosApplicationTests.java
+++ src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -2,17 +2,28 @@ package com.example.carros;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.carros.CarrosApplication;
+import com.carros.domain.Carro;
+import com.carros.domain.CarroService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CarrosApplication.class)
-class ApplicationTests {
+class CarrosApplicationTests {
 
+	@Autowired
+	private CarroService service;
+	
 	@Test
-	public void contextLoads() {
+	public void test1() {
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("esportivos");
+		
+		service.insert(carro);
 	}
 
 }
